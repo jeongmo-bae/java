a4e3fb7bf4689c2877fd00d3f270ab671c254826
jeongmo-bae <jeongmo-bae@github.com>
2024-11-26 13:11:12 +0900

    24.11.26

--- src/com/leetcode/studyplan/top150/arraynstring/RemoveDuplicatesfromSortedArrayII.java
+++ src/com/leetcode/studyplan/top150/arraynstring/RemoveDuplicatesfromSortedArrayII.java
@@ -2,14 +2,31 @@ package com.leetcode.studyplan.top150.arraynstring;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 //import java.util.ArrayList;
 //import java.util.List;
 
 //https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
 public class RemoveDuplicatesfromSortedArrayII {
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        return n;
+        HashMap<Integer,Integer> countMap = new HashMap<>();
+        for (Integer num : nums) {
+            if (countMap.getOrDefault(num, 0) != 2){
+                countMap.put(num, (countMap.getOrDefault(num, 0) + 1));
+            }
+        }
+        int k = 0;
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            int key = entry.getKey();   // 키 가져오기
+            int value = entry.getValue(); // 값 가져오기
+            k += value;
+            for (int i = k-value ; i < k ; i ++){
+                nums[i] = key;
+            }
+        }
+        System.out.println(countMap);
+        System.out.println(Arrays.toString(nums));
+        return k;
     }
 
 
@@ -37,19 +54,20 @@ public class RemoveDuplicatesfromSortedArrayII {
         RemoveDuplicatesfromSortedArrayII removeDup = new RemoveDuplicatesfromSortedArrayII();
 
         HashMap<String, Object> testCase = new HashMap<>();
-        testCase.put("numsArrayList", new int[][]{{1,1,1,2,2,3},{0,0,1,1,1,1,2,3,3}});
+        testCase.put("numsArrayList", new int[][]{{-3,-1,0,0,0,3,3},{0,0,1,1,1,1,2,3,3}});
         testCase.put("kArrayList", new int[]{5,7});
-        testCase.put("numsOutArrayList", new int[][]{{1,1,2,2,3},{0,0,1,1,2,3,3}});
+
         int cntTestCase = ((int[][]) testCase.get("numsArrayList")).length;
         for(int i=0;i<cntTestCase;i++){
             int[] nums = ((int[][]) testCase.get("numsArrayList"))[i];
 
             // 병합 메서드 실행
-            System.out.printf("##########%n[TestCase %d]%nInput nums : %s%n"
+            System.out.printf("##########%n[TestCase %d]%nInput nums : %s%nk = %d%n"
                     ,i+1
                     , Arrays.toString(nums)
+                    , removeDup.removeDuplicates(nums)
             );
-            removeDup.removeDuplicates(nums);
+
 
         }
     }
