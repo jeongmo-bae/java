4fb37aeb879efc64e1c55c5fc4d4368832d3849d
jeongmo-bae <jeongmo-bae@github.com>
2024-11-28 17:04:56 +0900

    24.11.28 ~ing

---
+++ src/com/leetcode/studyplan/top150/arraynstring/RotateArray.java
@@ -0,0 +1,14 @@
+package com.leetcode.studyplan.top150.arraynstring;
+//https://leetcode.com/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150
+
+import java.util.Arrays;
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] numsFront = new int[n-k];
+        int[] numsBack = new int[k];
+
+
+
+    }
+}
